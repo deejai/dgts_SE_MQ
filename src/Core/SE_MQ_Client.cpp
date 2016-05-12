ae5ffbe169b9c9df7885e3b58509eecbeefe0325
deejai <deejai@live.com>
2016-05-12 16:04:09 -0400

    Set up console interface to choose a Module

--- src/Core/SE_MQ_Client.cpp
+++ src/Core/SE_MQ_Client.cpp
@@ -1,28 +1,52 @@
 #pragma once
 #include "SE_MQ_Client.h"
 #include "ApiModule.h"
+#include "MunitionModule.h"
+#include "PhysicsModule.h"
 
 int main()
 {
 	ApiModule *apiMod = new ApiModule();
 	apiMod->run();
-
-	std::cout << "END_MAIN"; std::cin.ignore();
 	
-    // TODO: Display menu if no command line argument was received
-        // "Run SE_RabbitMQ_Client as:"
-        // "1. apiModule"
-        // "2. munitionModule"
-        // "3. physicsModule"
-        // "(API -> apiModule -> munitionModule -> physicsModule -> Environment)"
-        // Handle user input, loop if invalid
+	int choice = 0;
+	
+	while (choice < 1 || choice > 3)
+	{
+		// Display menu if no command line argument was received
+		std::cout << "Run SE_RabbitMQ_Client as:\n"
+				  << "1. Api Module\n"
+				  << "2. Munition Module\n"
+				  << "3. Physics Module\n"
+				  << "(API -> apiModule -> munitionModule -> physicsModule -> Environment)\n";
+		std::cin >> choice;
+			// TODO: Handle user input, loop if invalid
+	}
 
-    // TODO: Initialize chosen node
-        // Instantiate node
-        // Connect node's subscriber to input
-        // Connect node's publisher to output
-        // Start subscriber
-        // Start publisher
+	// Module
+	Module *mod;
+
+    // TODO: Initialize chosen module
+	switch (choice)
+	{
+	case 1:
+		mod = new ApiModule();
+		break;
+
+	case 2:
+		mod = new MunitionModule();
+		break;
+
+	case 3:
+		mod = new PhysicsModule();
+		break;
+
+	default:
+		std::cout << "This should never happen.\n";
+		return -1;
+	}
+
+	mod->run();
 
     return 0;
 }
